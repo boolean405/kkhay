477bf5924d8967229876066de2e44dad27ee97d5
Boolean <boolean405@gmail.com>
2025-06-21 07:11:22 +0700

    a

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,15 +1,24 @@
-import allowedOrigins from "./allowedOrigins.js";
+// import allowedOrigins from "./allowedOrigins.js";
 
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
+
+// export default corsOptions;
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    callback(null, origin || "*"); // Allow all origins dynamically
   },
   optionsSuccessStatus: 200,
-  credentials: true,
+  credentials: true, // Required for cookies/auth headers
 };
 
 export default corsOptions;
--- server/middlewares/credentials.js
+++ server/middlewares/credentials.js
@@ -1,9 +1,20 @@
-import allowedOrigins from "../config/allowedOrigins.js";
+// import allowedOrigins from "../config/allowedOrigins.js";
+
+// const credentials = (req, res, next) => {
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Credentials", "true");
+//   }
+//   next();
+// };
+
+// export default credentials;
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  if (origin) {
     res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", origin); // Dynamically set the origin
   }
   next();
 };
